362051408069faf32bb653f19e7abd7180046052
tupm-1335 <pham.minh.tu@sun-asterisk.com>
2021-01-08 15:00:10 +0700

    create UserController

--- src/main/kotlin/com/tupm/crud_user_demo/repository/UserRepository.kt
+++ src/main/kotlin/com/tupm/crud_user_demo/repository/UserRepository.kt
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-class UserRepository {
-  interface UserRepository : JpaRepository<User, Long>
-}
+interface UserRepository : JpaRepository<User, Long>
 //extended UserRepository from JpaRepository interface, all the CRUD methods on User entity is ready available
