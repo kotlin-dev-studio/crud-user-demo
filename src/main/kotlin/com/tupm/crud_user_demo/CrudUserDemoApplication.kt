dde91186b7e316f29d34cb59a38c2ee84f005e60
tupm-1335 <pham.minh.tu@sun-asterisk.com>
2021-01-08 12:50:45 +0700

    Init project

---
+++ src/main/kotlin/com/tupm/crud_user_demo/CrudUserDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.tupm.crud_user_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudUserDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudUserDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/tupm/crud_user_demo/CrudUserDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tupm.crud_user_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudUserDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
